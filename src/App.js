5549eea66fea5258adba4db1e862a90bd40b5b97
AntonioQuintero <aquintero@magnasistemas.com.br>
2022-08-26 12:31:33 -0300

    Projeto React Hooks Na Pratica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App(){
+
+  const [location, setLocation ] = useState({});
+
+  useEffect(() =>{
+    const watchID = navigator.geolocation.watchPosition(handlePositionReceived)
+    return () => navigator.geolocation.clearWatch(watchID);
+  }, []);
+
+  function handlePositionReceived({coords}){
+    const {latitude, longitude} = coords;
+    setLocation({latitude, longitude});
+  }
+
+  // const [repositories, setRepositories] = useState([]);
+
+  // useEffect(()=>{
+  //   const filtered = repositories.filter(repo => repo.favorite);
+  //   document.title = `Você tem ${filtered.length} favoritos`
+  // }, [repositories]);
+
+  // useEffect(async () => {
+  //   const response = await fetch('https://api.github.com/users/diego3g/repos');
+  //   const data = await response.json();
+  //   setRepositories(data)
+  // },[]);
+
+  // function handleFavorite (id){
+  //   const newRepositories = repositories.map(repo => {
+  //     return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
+  //   });
+  //   setRepositories(newRepositories);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude}
+    </>
   );
 }
-
-export default App;
